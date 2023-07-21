19ea812d9af6ae341a639bbc9c6292602b9eca62
NHclub <sbe4488@naver.com>
2023-07-21 20:36:42 +0900

    init commit

---
+++ src/test/java/com/example/newnique/NewNiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewNiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
