65a08e2e4a0e859d3d65f51fd1f40b18a88d1b35
KIM MIN GYU <mingyugim584@gmail.com>
2023-07-25 18:12:40 +0900

    fix: 네이티브 쿼리 수정

--- src/main/java/com/example/newnique/news/repository/NewsRepository.java
+++ src/main/java/com/example/newnique/news/repository/NewsRepository.java
@@ -18,7 +18,7 @@ public interface NewsRepository extends JpaRepository<News,Long> {
 
     @Query(
             value =
-                    "SELECT * FROM News WHERE MATCH(title, content) AGAINST (:keyword) " +
+                    "SELECT * FROM news WHERE MATCH(title, content) AGAINST (:keyword) " +
                             "ORDER BY :orderField :orderDirection " +
                             "LIMIT :limit OFFSET :offset", nativeQuery = true
     )
