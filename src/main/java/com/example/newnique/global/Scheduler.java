746b645733e8e878a4c62cd04f9c7bcf0c080298
KIM MIN GYU <84438772+kmg159753@users.noreply.github.com>
2023-07-26 18:11:50 +0900

    style: 주석 처리

--- src/main/java/com/example/newnique/global/Scheduler.java
+++ src/main/java/com/example/newnique/global/Scheduler.java
@@ -41,10 +41,10 @@ public class Scheduler {
         }
     }
 
-    @PostConstruct
-    public void init() throws InterruptedException {
-        updateNews(); // 프로그램 시작 시에 한 번 실행(테스트 용도)
-    }
+    // @PostConstruct
+    // public void init() throws InterruptedException {
+    //     updateNews(); // 프로그램 시작 시에 한 번 실행(테스트 용도)
+    // }
 
     @Scheduled(cron = "0 0 3 * * ?")
     public void updateNews() throws InterruptedException {
