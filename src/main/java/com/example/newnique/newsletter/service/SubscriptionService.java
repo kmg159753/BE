1445f6b6b55261e36db03e302f156fcc13335ca5
DESKTOP-LOH5O20\rehit <rehiter48@gmail.com>
2023-07-27 14:49:27 +0900

    feat: email 수정

--- src/main/java/com/example/newnique/newsletter/service/SubscriptionService.java
+++ src/main/java/com/example/newnique/newsletter/service/SubscriptionService.java
@@ -1,5 +1,7 @@
 package com.example.newnique.newsletter.service;
 
+import com.example.newnique.news.entity.News;
+import com.example.newnique.news.repository.NewsRepository;
 import com.example.newnique.newsletter.dto.Maildto;
 import com.example.newnique.newsletter.dto.SubscriptionRequestDto;
 import com.example.newnique.newsletter.entity.Subscription;
@@ -14,13 +16,15 @@ import org.springframework.stereotype.Service;
 public class SubscriptionService {
 
     private final SubscriptionRepository subscriptionRepository;
+    private final NewsRepository newsRepository;
     private final EmailSender emailSender;
     public Subscription createSub(SubscriptionRequestDto subscriptionRequestDto) throws MessagingException {
         Subscription subscription = new Subscription(subscriptionRequestDto);
         Subscription saveSubscription = subscriptionRepository.save(subscription);
+        News news = newsRepository.findTopByOrderByIdDesc();
 
         Maildto maildto = Maildto.builder().title(subscriptionRequestDto.getNickName()+"님 환영합니다")
-                .message("뉴닉을 구독해주신걸 환영합니다 !")
+                .message("뉴닉을 구독해주신걸 환영합니다 !\n\n\n"+news.getTitle() + "\n\n\n"+news.getNewsSummary())
                 .toAddress(subscriptionRequestDto.getEmail()).build();
         emailSender.sendMail(maildto);
 
